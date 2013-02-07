1013b3f3d9f3a7082584094eadb3f2504299e446
Alex Mompo Mikkelsen <alex_mompo@yahoo.com>
2013-02-07 18:53:50 +0100

    first reelase

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,9 +1,12 @@
 class PropertiesController < ApplicationController
 	def show
 		@name = params[:id]
+		render layout: 'single_item'
 	end
 
 	def index
+		@properties = Property.all
+		render layout: 'list'
 	end
 
 	def new
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,10 +1,38 @@
 class Property
 	include ::ActiveModel::Model
 
-	attr_accessor :name, :cost, :city
+	attr_accessor :name, :cost, :city, :id
+
+	def id
+		1
+	end
 
 	def self.featured_ads count = 6
 		count.times.map { Property.new name: random_name, cost: random_cost, city: 'Copenhagen '}
+	end
+
+	def self.all count = 10
+		rand(count+1).times.map { Property.new name: random_name, cost: random_cost, city: 'Copenhagen '}
+	end
+
+	def mail
+		@mail ||= Mail.new
+	end
+
+	def favorite
+		@favorite ||= Favorite.new
+	end
+
+	class Mail
+		def status
+			'unread'
+		end
+	end
+
+	class Favorite
+		def status
+			'on'
+		end
 	end
 
 	protected
