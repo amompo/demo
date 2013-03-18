a76471dd47624493e9c9e7785a5017782deaa50a
Kristian Mandrup <kmandrup@gmail.com>
2013-03-18 13:09:27 +0100

    delete accounts

--- app/models/account/landlord.rb
+++
@@ -1,7 +0,0 @@
-module Account
-  class Landlord < User
-    include BasicDocument
-
-    embedded_in :account, polymorphic: true
-  end
-end
--- app/models/account/tenant.rb
+++
@@ -1,7 +0,0 @@
-module Account
-  class Tenant < User
-    include BasicDocument
-
-    embedded_in :account, polymorphic: true
-  end
-end
